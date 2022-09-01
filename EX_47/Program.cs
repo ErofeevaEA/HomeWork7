b0546255405f3715c8c4db347f5db6acbdf1bd4a
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-09-01 21:18:13 +0300

    Exercise 47

---
+++ EX_47/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        double[,] a = new double[3, 4];
+
+        Random random = new Random();
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                a[i, j] = random.NextDouble() * 100;
+                Console.Write("{0,6:F2}", a[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
